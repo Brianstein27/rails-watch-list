1594ee47e4b3be7f4e242ab7d3f16c0323ffda91
Brian Sauls <brianstein27@proton.me>
2024-05-16 12:37:37 +0200

    Refactor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,7 +20,7 @@ movies['results'].each do |movie|
   new_movie = Movie.new(
     title: movie['title'],
     overview: movie['overview'],
-    poster_url: movie['poster_path'],
+    poster_url: "https://image.tmdb.org/t/p/original#{movie['poster_path']}",
     rating: movie['vote_average']
   )
 
